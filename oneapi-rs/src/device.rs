53af259c559bd39804c98cc9709b5e8a9edf09d1
Szymon Zadworny <szymon.zadworny@intel.com>
2026-07-29 10:12:53 +0000

    Refactor Device to use new Info trait

--- oneapi-rs/src/device.rs
+++ oneapi-rs/src/device.rs
@@ -8,12 +8,14 @@
 
 use oneapi_rs_sys::device::ffi;
 
-use crate::{info::device::DeviceInfo, platform::Platform};
+use crate::{info::InfoTarget, platform::Platform};
 
 /// The `Device` struct encapsulates a single SYCL device on which kernels can be executed.
 ///
 /// The `Device` struct provides the common reference semantics.
 pub struct Device(pub(crate) cxx::UniquePtr<ffi::Device>);
+
+impl InfoTarget for Device {}
 
 impl From<cxx::UniquePtr<ffi::Device>> for Device {
     fn from(value: cxx::UniquePtr<ffi::Device>) -> Self {
@@ -29,14 +31,6 @@ impl Device {
             .into_iter()
             .map(|device| Self(device.ptr))
             .collect()
-    }
-
-    /// Queries this `Device` for information requested by the generic parameter `Param`.
-    /// The associated type `Param::Item` must be defined in accordance with the info parameters
-    /// in [`oneapi_rs::info::device`](`crate::info::device`) to facilitate returning the type
-    /// associated with the `Param` parameter.
-    pub fn get_info<T: DeviceInfo>(&self) -> T::Item {
-        T::get_item(self)
     }
 
     /// Returns the associated SYCL platform.
