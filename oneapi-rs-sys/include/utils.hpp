0e7ac3ffbe8ec66be6aa6bdfe21713fcac7301a1
Szymon Zadworny <szymon.zadworny@intel.com>
2026-07-30 10:45:32 +0000

    Add rust::Vec to std::vector conversion function

--- oneapi-rs-sys/include/utils.hpp
+++ oneapi-rs-sys/include/utils.hpp
@@ -9,9 +9,23 @@
 #pragma once
 
 #include <memory>
+#include <type_traits>
+#include <utility>
+#include <vector>
 
 #include "oneapi-rs-sys/include/types.hpp"
 #include "rust/cxx.h"
 
-namespace sycl_shims {
-} // namespace sycl_shims
+namespace sycl_shims::utils {
+template <typename T>
+using UnwrappedPtr = std::remove_reference_t<decltype(*std::declval<T>().ptr)>;
+
+template <typename T>
+std::vector<UnwrappedPtr<T>> vec_to_vector(rust::Vec<T> &&vec) {
+  std::vector<UnwrappedPtr<T>> vector;
+  for (auto &&e : vec)
+    vector.push_back(std::move(*e.ptr));
+
+  return vector;
+}
+} // namespace sycl_shims::utils
